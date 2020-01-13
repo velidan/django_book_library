8f61d45776d33a9fe8d90dbd655da22812f062f2
Velidan <wellnineweb@gmail.com>
2020-01-13 19:06:19 +0200

    - DRF: Book CRUD

--- catalog/views.py
+++ catalog/views.py
@@ -18,7 +18,7 @@ from catalog.serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import authentication, permissions
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 
 def index(request):
     """View function for home page of site."""
@@ -175,13 +175,37 @@ class BooksRest(APIView):
         print(serializer.data)
         return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
 class BookRest(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(id=pk)
+        except Book.DoesNotExist:
+            raise Http404
+
     def get(self, request, pk, format=None):
         """Retrieve a particular book"""
-        try:
-            book = Book.objects.get(id=pk)
-        except:
-            return HttpResponse(status=404)
+        book = self.get_object(pk)
 
         serializer = BookSerializer(book)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, pk, format=None):
+        book = self.get_object(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
